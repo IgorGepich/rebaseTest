7d08b7c9a711e0018f334a5633a82a5d21500989
ihors-techspark <ihor.s@techspark.engineering>
2023-07-16 18:23:27 +0300

    3
    Signed-off-by: ihors-techspark <ihor.s@techspark.engineering>

--- src/main/java/RebaseSecond.java
+++ src/main/java/RebaseSecond.java
@@ -5,10 +5,10 @@ public class RebaseSecond {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("");
+
         System.out.println("5");
 
-        System.out.println("3");
+        System.out.println("2");
 
-        System.out.println("Hello Moon");
     }
 }
