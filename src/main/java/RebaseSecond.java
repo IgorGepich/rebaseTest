fa9793786ce4d74131d1ac8ee51913d3ea54bb44
ihors-techspark <ihor.s@techspark.engineering>
2023-07-16 19:59:19 +0300

    1-4
    Signed-off-by: ihors-techspark <ihor.s@techspark.engineering>
    
    add 5 line
    
    Signed-off-by: ihors-techspark <ihor.s@techspark.engineering>

--- src/main/java/RebaseSecond.java
+++ src/main/java/RebaseSecond.java
@@ -6,5 +6,12 @@ public class RebaseSecond {
 
         System.out.println("1");
         System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+<<<<<<< HEAD
+=======
+        System.out.println("5");
+
+>>>>>>> 1805dfb... add 5 line
     }
 }
